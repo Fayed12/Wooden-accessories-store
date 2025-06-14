bbbe13c4de2ee96bb16c120c7f7a59a0101b4443
mohamed fayed <memad4858@gmail.com>
2025-06-14 20:09:01 +0300

    modifie home page

--- main.js
+++ main.js
@@ -1,11 +1,26 @@
-// functionalty of barContent
+// functionalty of bar Content
+
 let barContent = document.getElementById("barContent")
 let spanOne = document.getElementById("spanOne")
 let spanTwo = document.getElementById("spanTwo")
 let spanThree = document.getElementById("spanThree")
+let menu = document.getElementById("menu");
 
 barContent.addEventListener("click", () => {
     spanOne.classList.toggle("left");
     spanTwo.classList.toggle("hide");
     spanThree.classList.toggle("right");
+    menu.classList.toggle("display")
+})
+
+document.addEventListener("click", (e) => {
+    if (menu.classList.contains("display")) {
+        // Check if click is outside the navbar and button
+        if (!barContent.contains(e.target) && (!menu.contains(e.target))) {
+            menu.classList.toggle("display")
+            spanOne.classList.toggle("left");
+            spanTwo.classList.toggle("hide");
+            spanThree.classList.toggle("right");
+        }
+    };
 })
